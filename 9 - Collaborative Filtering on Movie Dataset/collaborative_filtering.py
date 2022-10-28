e71f1f05b8076782828ea5bd1b25c187da0b13e4
Ced <79917173+Revkxd@users.noreply.github.com>
2022-10-28 15:47:57 +0800

    Notebook 9

---
+++ 9 - Collaborative Filtering on Movie Dataset/collaborative_filtering.py
@@ -0,0 +1,182 @@
+import numpy as np
+import pandas as pd
+
+
+class CollaborativeFiltering(object):
+
+    def __init__(self, k):
+        """Class constructor for KMeans
+        Arguments:
+            k {int} -- number of similar items to consider
+        """
+        self.k = k
+
+    def get_row_mean(self, data):
+        """Returns the mean of each row in the DataFrame or the mean of the
+        Series. If the parameter data is a DataFrame, the function will
+        return a Series containing the mean of each row in the DataFrame. If
+        the parameter data is a Series, the function will return a np.float64
+        which is the mean of the Series. This function should not consider
+        blank ratings represented as NaN.
+
+        Arguments:
+            data {DataFrame or Series} -- dataset
+        Returns:
+            Series or np.float64 -- row mean
+        """
+
+        # TODO: Implement this function based on the documentation.
+
+        # TODO: Check if the parameter data is a Series or a DataFrame
+        if len(data.shape) == 1:
+            axis = 0
+        else:
+            axis = 1
+
+        # TODO: return the mean of each row if the parameter data is a
+        # DataFrame. Return the mean of the Series if the parameter data is a
+        # Series.
+        # Hint: Use pandas.DataFrame.mean() or pandas.Series.mean() functions.
+        return data.mean(axis=axis)
+
+    def normalize_data(self, data, row_mean):
+        """Returns the data normalized by subtracting the row mean.
+
+        For the arguments point1 and point2, you can only pass these
+        combinations of data types:
+        - DataFrame and Series -- returns DataFrame
+        - Series and np.float64 -- returns Series
+
+        For a DataFrame and a Series, if the shape of the DataFrame is
+        (3, 2), the shape of the Series should be (3,) to enable broadcasting.
+        This operation will result to a DataFrame of shape (3, 2)
+
+        Arguments:
+            data {DataFrame or Series} -- dataset
+            row_mean {Series or np.float64} -- mean of each row
+        Returns:
+            DataFrame or Series -- normalized data
+        """
+
+        # TODO: Implement this function based on the documentation.
+
+        # TODO: Check if the combination of parameters is correct
+        # Normalize the parameter data by parameter row_mean.
+        # HINT: Use pandas.DataFrame.subtract() or pandas.Series.subtract()
+        # functions.
+        return data.subtract(row_mean, axis=0)
+
+    def get_cosine_similarity(self, vector1, vector2):
+        """Returns the cosine similarity between two vectors. These vectors can
+        be represented as 2 Series objects. This function can also compute the
+        cosine similarity between a list of vectors (represented as a
+        DataFrame) and a single vector (represented as a Series), using
+        broadcasting.
+
+        For the arguments vector1 and vector2, you can only pass these
+        combinations of data types:
+        - Series and Series -- returns np.float64
+        - DataFrame and Series -- returns pd.Series
+
+        For a DataFrame and a Series, if the shape of the DataFrame is
+        (3, 2), the shape of the Series should be (2,) to enable broadcasting.
+        This operation will result to a Series of shape (3,)
+
+        Arguments:
+            vector1 {Series or DataFrame} - vector
+            vector2 {Series or DataFrame} - vector
+        Returns:
+            np.float64 or pd.Series -- contains the cosine similarity between
+            two vectors
+        """
+
+        # TODO: Implement this function based on the documentation.
+
+        # TODO: Check if the parameter data is a Series or a DataFrame
+
+        # TODO: Compute the cosine similarity between the two parameters.
+        # HINT: Use np.sqrt() and pandas.DataFrame.sum() and/or
+        # pandas.Series.sum() functions.
+        axis = 0 if len(vector1.shape) == 1 and len(vector2.shape) == 1 else 1
+        sum_of_products = (vector1 * vector2).sum(axis=axis)
+
+        axis = 0  if len(vector1.shape) == 1 else 1
+        vector1_sss = np.sqrt((vector1 ** 2).sum(axis=axis))
+
+        axis = 0 if len(vector2.shape) == 1 else 1
+        vector2_sss = np.sqrt((vector2 ** 2).sum(axis=axis))
+
+        return sum_of_products / (vector1_sss * vector2_sss)
+
+    def get_k_similar(self, data, vector):
+        """Returns two values - the indices of the top k similar items to the
+        vector from the dataset, and a Series representing their similarity
+        values to the vector. We find the top k items from the data which
+        are highly similar to the vector.
+
+        Arguments:
+            data {DataFrame} -- dataset
+            vector {Series} -- vector
+        Returns:
+            Index -- indices of the top k similar items to the vector
+            Series -- computed similarity of the top k similar items to the
+            vector
+        """
+
+        # TODO: Implement this function based on the documentation.
+
+        # TODO: Normalize parameters data and vector
+        # HINT: Use the normalize_data() function that we have defined in this
+        # class
+        n_data = self.normalize_data(data, self.get_row_mean(data))
+        n_vector = self.normalize_data(vector, self.get_row_mean(vector))
+
+        # TODO: Get the cosine similarity between the normalized data and
+        # vector
+        # HINT: Use the get_cosine_similarity() function that we have defined
+        # in this class
+        similarity_vector = self.get_cosine_similarity(n_data, n_vector)
+
+        # TODO: Get the INDICES of the top k most similar items based on
+        # the cosine similarity values
+        # HINT: Use pandas.Series.nlargest() function.
+        k_similar = similarity_vector.nlargest(n=self.k).index
+
+        # TODO: Return 2 values. See function comment
+        return k_similar, similarity_vector.loc[k_similar]
+
+    def get_rating(self, data, index, column):
+        """Returns the extrapolated rating for the item in row index from the
+        user in column column based on similar items.
+
+        The algorithm for this function is as follows:
+        1. Get k similar items.
+        2. Compute for the rating using the similarity values and the raw
+        ratings for the k similar items.
+
+        Arguments:
+            data {DataFrame} -- dataset
+            index {int} -- row of the item
+            column {int} -- column of the user
+        Returns:
+            np.float64 -- extrapolated rating based on similar ratings
+        """
+
+        # TODO: Complete this function.
+
+        before_df = data.iloc[:index, :]
+        after_df = data.iloc[index + 1:, :]
+        new_data = pd.concat([before_df, after_df])
+        vector = data.iloc[index, :]
+
+        # TODO: Get top k items that are similar to the parameter vector
+        # HINT: Use the get_k_similar() function that we have defined in this
+        # class
+        k_similar, k_similarity_values = self.get_k_similar(new_data, vector)
+        k_ratings = data.loc[k_similar].iloc[:, column]
+
+        # TODO: Compute for the rating using the similarity values and the raw
+        # ratings for the k similar items.
+        rating = (k_similarity_values * k_ratings).sum() / k_similarity_values.sum()
+
+        return rating
